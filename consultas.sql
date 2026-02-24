a8ce56c01ee5c9219303e8d99f4b8ddca834ff3e
alicecaetano090518 <alicecaetano090518@gmail.com>
2026-02-24 12:09:33 -0300

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -2,3 +2,30 @@
 SELECT a.nome, a.matricula, c.id_cartao, a.tipo
 FROM aluno a
 JOIN cartao c ON a.id_aluno = c.id_aluno;
+
+-- Listar alunos com seus cartões
+SELECT a.nome, a.matricula, c.id_cartao
+FROM aluno a
+JOIN cartao c ON a.id_aluno = c.id_aluno;
+
+
+-- Ver quais alunos ainda não têm cartão
+SELECT a.nome
+FROM aluno a
+LEFT JOIN cartao c ON a.id_aluno = c.id_aluno
+WHERE c.id_cartao IS NULL;
+
+
+-- Mostrar todos os empréstimos feitos
+SELECT a.nome, e.data_emprestimo, e.est_data_devolucao
+FROM emprestimo e
+JOIN aluno a ON e.id_aluno = a.id_aluno;
+
+
+-- Calcular o saldo total de cada cartão
+SELECT c.id_cartao, SUM(r.valor) AS saldo
+FROM cartao c
+JOIN recarga r ON c.id_cartao = r.id_cartao
+GROUP BY c.id_cartao;
+
+
